de60b438bcc869a4226119487730aafcdcd5bfee
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-02-15 16:38:41 -0500

    extend cofe-ct-commerce

---
+++ actionControllers/AccountController.ts
@@ -0,0 +1,243 @@
+import { Request, Response } from '@frontastic/extension-types';
+import { ActionContext } from '@frontastic/extension-types';
+import { AccountApi } from '../apis/AccountApi';
+import { AccountExtended as Account } from 'cofe-ct-ecommerce/interfaces/AccountExtended';
+import { Address } from '@commercetools/frontend-domain-types/account/Address';
+import { CartFetcher } from 'cofe-ct-ecommerce/utils/CartFetcher';
+export * from 'cofe-ct-ecommerce/actionControllers/AccountController';
+import { getLocale } from 'cofe-ct-ecommerce/utils/Request';
+import {parseBirthday} from 'cofe-ct-ecommerce/utils/parseBirthday';
+import { EmailApi } from 'cofe-ct-ecommerce/apis/EmailApi';
+import { BusinessUnitApi } from '../apis/BusinessUnitApi';
+import { NotificationApi } from '../apis/NotificationApi';
+import { assertIsAuthenticated } from 'cofe-ct-ecommerce/utils/assertIsAuthenticated';
+import { fetchAccountFromSession } from 'cofe-ct-ecommerce/actionControllers/fetchAccountFromSession';
+
+type ActionHook = (request: Request, actionContext: ActionContext) => Promise<Response>;
+
+export type AccountRegisterBody = {
+  email?: string;
+  confirmed?: boolean;
+  password?: string;
+  salutation?: string;
+  firstName?: string;
+  lastName?: string;
+  company?: string;
+  birthdayYear?: string;
+  birthdayMonth?: string;
+  birthdayDay?: string;
+  billingAddress?: Address;
+  shippingAddress?: Address;
+};
+
+type AccountLoginBody = {
+  email?: string;
+  password?: string;
+};
+
+async function loginAccount(request: Request, actionContext: ActionContext, account: Account, reverify = false) {
+  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request));
+  const businessUnitApi = new BusinessUnitApi(actionContext.frontasticContext, getLocale(request));
+  const notificationApi = new NotificationApi(actionContext.frontasticContext, getLocale(request));
+
+  const cart = await CartFetcher.fetchCart(request, actionContext);
+
+  try {
+    const accountRes = await accountApi.login(account, cart, reverify);
+    const organization = await businessUnitApi.getOrganization(accountRes.accountId);
+    const token = await notificationApi.getToken(account.email, account.password);
+
+    return { account: accountRes, organization, token };
+  } catch (e) {
+    throw e;
+  }
+}
+
+function mapRequestToAccount(request: Request): Account {
+  const accountRegisterBody: AccountRegisterBody = JSON.parse(request.body);
+
+  const account: Account = {
+    email: accountRegisterBody?.email ?? '',
+    confirmed: accountRegisterBody?.confirmed,
+    password: accountRegisterBody?.password,
+    salutation: accountRegisterBody?.salutation,
+    firstName: accountRegisterBody?.firstName,
+    lastName: accountRegisterBody?.lastName,
+    company: accountRegisterBody?.company,
+    birthday: parseBirthday(accountRegisterBody),
+    addresses: [],
+  };
+
+  if (accountRegisterBody.billingAddress) {
+    accountRegisterBody.billingAddress.isDefaultBillingAddress = true;
+    accountRegisterBody.billingAddress.isDefaultShippingAddress = !(accountRegisterBody.shippingAddress !== undefined);
+
+    account.addresses!.push(accountRegisterBody.billingAddress);
+  }
+
+  if (accountRegisterBody.shippingAddress) {
+    accountRegisterBody.shippingAddress.isDefaultShippingAddress = true;
+    accountRegisterBody.shippingAddress.isDefaultBillingAddress = !(accountRegisterBody.billingAddress !== undefined);
+
+    account.addresses!.push(accountRegisterBody.shippingAddress);
+  }
+
+  return account;
+}
+
+export const register: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request));
+  const emailApi = new EmailApi(actionContext.frontasticContext.project.configuration.smtp);
+
+  const accountData = mapRequestToAccount(request);
+  const host = JSON.parse(request.body).host;
+
+  const cart = await CartFetcher.fetchCart(request, actionContext).catch(() => undefined);
+
+  let response: Response;
+
+  try {
+    const account = await accountApi.create(accountData, cart);
+
+    if (!account.confirmed) await emailApi.sendVerificationEmail(account, host);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ accountId: account.accountId }),
+      sessionData: {
+        ...request.sessionData,
+      },
+    };
+  } catch (e) {
+    response = {
+      statusCode: 400,
+      // @ts-ignore
+      error: e?.message,
+      errorCode: 500,
+    };
+  }
+  return response;
+};
+
+export const login: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  const accountLoginBody: AccountLoginBody = JSON.parse(request.body);
+  const config = actionContext.frontasticContext?.project?.configuration?.storeContext;
+
+  const loginInfo = {
+    email: accountLoginBody.email,
+    password: accountLoginBody.password,
+  } as Account;
+
+  let response: Response;
+
+  try {
+    const { account, organization, token } = await loginAccount(request, actionContext, loginInfo);
+    console.debug('set in session', token);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(account),
+      sessionData: {
+        ...request.sessionData,
+        account,
+        organization,
+        // @ts-ignore
+        rootCategoryId: organization.store?.custom?.fields?.[config?.rootCategoryCustomField]?.id,
+        notificationToken: token,
+      },
+    };
+  } catch (e) {
+    response = {
+      statusCode: 400,
+      // @ts-ignore
+      error: e?.message,
+      errorCode: 500,
+    };
+  }
+
+  return response;
+};
+
+export const logout: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+    sessionData: {
+      ...request.sessionData,
+      organization: undefined,
+      account: undefined,
+    },
+  } as Response;
+};
+
+
+/**
+ * Reset password
+ */
+export const reset: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  type AccountResetBody = {
+    token?: string;
+    newPassword?: string;
+  };
+
+  const accountResetBody: AccountResetBody = JSON.parse(request.body);
+
+  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request));
+
+  const newAccount = await accountApi.resetPassword(accountResetBody.token, accountResetBody.newPassword);
+  newAccount.password = accountResetBody.newPassword;
+
+  // TODO: do we need to log in the account after creation?
+  // TODO: handle exception when customer can't login if email is not confirmed
+  const { account, organization } = await loginAccount(request, actionContext, newAccount);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(account),
+    sessionData: {
+      ...request.sessionData,
+      account,
+      organization,
+    },
+  } as Response;
+};
+
+export const addAddress: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  assertIsAuthenticated(request);
+
+  let account = fetchAccountFromSession(request);
+
+  const address: Address = JSON.parse(request.body);
+
+  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request));
+
+  account = await accountApi.addAddress(account, address);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(account),
+    sessionData: {
+      ...request.sessionData,
+      account,
+    },
+  } as Response;
+};
+
+export const updateAddress: ActionHook = async (request: Request, actionContext: ActionContext) => {
+  assertIsAuthenticated(request);
+
+  let account = fetchAccountFromSession(request);
+
+  const address: Address = JSON.parse(request.body);
+
+  const accountApi = new AccountApi(actionContext.frontasticContext, getLocale(request));
+
+  account = await accountApi.updateAddress(account, address);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(account),
+    sessionData: {
+      ...request.sessionData,
+      account,
+    },
+  } as Response;
+};
---
+++ apis/AccountApi.ts
@@ -0,0 +1,67 @@
+
+import { AccountMapper } from 'cofe-ct-ecommerce/mappers/AccontMapper';
+import { CartResourceIdentifier } from '@commercetools/platform-sdk/dist/declarations/src/generated/models/cart';
+import {AccountApi as CTAccountApi} from 'cofe-ct-ecommerce/apis/AccountApi'
+import { Account } from '@commercetools/frontend-domain-types/account/Account';
+import { Cart } from '@commercetools/frontend-domain-types/cart/Cart';
+
+export class AccountApi extends CTAccountApi {
+
+  login: (account: Account, cart: Cart | undefined, reverify?: boolean) => Promise<Account> = async (
+    account: Account,
+    cart: Cart | undefined,
+    reverify = false,
+  ) => {
+    try {
+      const locale = await this.getCommercetoolsLocal();
+
+      account = await this.getApiForProject()
+        .login()
+        .post({
+          body: {
+            email: account.email,
+            password: account.password!,
+            anonymousCart:
+              cart !== undefined
+                ? ({
+                    typeId: 'cart',
+                    id: cart.cartId,
+                  } as CartResourceIdentifier)
+                : undefined,
+          },
+        })
+        .execute()
+        .then((response) => {
+          return AccountMapper.commercetoolsCustomerToAccount(response.body.customer, locale);
+        })
+        .catch((error) => {
+          if (error.code && error.code === 400) {
+            if (error.body && error.body?.errors?.[0]?.code === 'InvalidCredentials') {
+              throw new Error(`Invalid credentials to login with the account ${account.email}`);
+            }
+
+            /*
+             * The cart might already belong to another user, so we try to log in without the cart.
+             */
+            if (cart) {
+              return this.login(account, undefined, reverify);
+            }
+          }
+
+          throw new Error(`Failed to login account  ${account.email}.`);
+        });
+
+      if (reverify) {
+        const token = await this.getConfirmationToken(account);
+        account.confirmationToken = token;
+      } else if (!account.confirmed) {
+        throw new Error(`Your account ${account.email} is not activated yet!`);
+      }
+
+      return account;
+    } catch (error) {
+      //TODO: better error, get status code etc...
+      throw error;
+    }
+  };
+}
