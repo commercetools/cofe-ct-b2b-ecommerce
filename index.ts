51874744bdc5f9df99fbc78b31bdb05fe1d98d72
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-02-16 08:18:39 -0500

    extender
    ext
    
    nextender
    
    ts

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-import {  } from 'actionControllers/AccountController';
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+import * as AccountSourceActions from './actionControllers/AccountController';
+
+export const extender= <T>(sourcePath: string, target: T): T => {
+  const source = require(sourcePath);
+  if (!target) {
+    return source;
+  }
+  return {
+    ...source,
+    ...target,
+  };
+};
+
+export const AccountAction = extender<typeof AccountSourceActions>('cofe-ct-ecommerce/actionControllers/AccountController', AccountSourceActions);
