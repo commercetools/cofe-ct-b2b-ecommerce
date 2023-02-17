f7934553635a4d25bb82a2c2dfd2fba3ce163f3d
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-02-17 09:26:30 -0500

    fsdads

--- index.ts
+++ index.ts
@@ -1,18 +1,15 @@
-import {
-  AccountController as AccountControllerBase,
-  AccountControllerType as AccountControllerBaseType,
-} from 'cofe-ct-ecommerce/actionControllers/AccountController';
-import { AccountController, AccountControllerType } from './actionControllers/AccountController';
+import * as AccountControllerBase  from 'cofe-ct-ecommerce/actionControllers/AccountController';
+import * as AccountController from './actionControllers/AccountController';
 
-export const extender = (args: object) => (source: any, target: any) => {
+export const extender = (source: any, target: any) => {
   if (!target) {
     return source;
   }
   return {
-    ...source(args),
-    ...target(args),
+    ...source,
+    ...target,
   };
 };
 
-export const AccountAction: (args: object) => AccountControllerType & AccountControllerBaseType = (args: object) =>
-  extender(args)(AccountControllerBase, AccountController);
+export const AccountAction: typeof AccountController & typeof AccountControllerBase =
+  extender(AccountControllerBase, AccountController);
