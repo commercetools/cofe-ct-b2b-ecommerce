ea04aa9bcbbc962afa46d47a33656b1a84d6bacf
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-03-03 11:40:33 -0500

    5

--- types/cart/LineItem.ts
+++ types/cart/LineItem.ts
@@ -1,4 +1,5 @@
-import { LineItem as DomainLineItem  } from '@commercetools/frontend-domain-types/cart/LineItem';
+import { LineItem as DomainLineItem } from '@commercetools/frontend-domain-types/cart/LineItem';
+import { Variant } from '../product/Variant';
 
 export interface LineItemReturnItemDraft {
   quantity: number;
@@ -12,6 +13,7 @@ export interface Target {
 }
 
 export interface LineItem extends DomainLineItem {
+  variant: Variant;
   custom?: Record<string, any>;
   shippingDetails?: {
     targets?: Target[];
