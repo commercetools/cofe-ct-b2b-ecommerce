9cb7ab8d5b4308ce9950de35e9b398e217911fe8
Behnam Tehrani <behnam.tehrani@commercetools.com>
2023-05-16 14:38:59 -0400

    Revert "test gql"
    This reverts commit ee9083efbc1dfab2a32552380795e8212f2ca867.

--- queries/ProductProjectionSearch.ts
+++ queries/ProductProjectionSearch.ts
@@ -5,28 +5,17 @@ import { fragmentDiscountedPrice } from './fragments/fragmentDiscountedPrice';
 import { fragmentFrSearchQuery } from './fragments/fragmentFrSearchQuery';
 import { fragmentPrices } from './fragments/fragmentPrices';
 import { fragmentProductVariant } from './fragments/fragmentProductVariant';
-import * as gql from 'gql-query-builder'
 
-// const query = `
-//   query GetProducts($locale: Locale, $currency: Currency!) {
-//     productProjectionSearch {
-//       ...FrSearchQuery
-//       ...CurrentProduct
-//     }
-//   }
-// `;
+const query = (parameters: Record<string, any>) => `
+  query GetProducts($locale: Locale, $currency: Currency!) {
+    productProjectionSearch(${parametersToString(parameters)}) {
+      ...FrSearchQuery
+      ...CurrentProduct
+    }
+  }
+`;
 
-// export const productProjectionSearchQuery = (parameters: Record<string, any>) =>
-//   `${fragmentDiscountedPrice}${fragmentPrices}${fragmentProductVariant}${fragmentFrSearchQuery}${fragmentCategories}${fragmentCurrentProduct}${query(
-//     parameters,
-//   )}`;
-
-export const productProjectionSearchQuery = (variables, parameters) => gql.query({
-  operation:'productProjectionSearch',
-  fields: [
-    'offset',
-    'count',
-    'total',
-  ],
-  variables: {...variables, ...parameters}
-})
+export const productProjectionSearchQuery = (parameters: Record<string, any>) =>
+  `${fragmentDiscountedPrice}${fragmentPrices}${fragmentProductVariant}${fragmentFrSearchQuery}${fragmentCategories}${fragmentCurrentProduct}${query(
+    parameters,
+  )}`;
